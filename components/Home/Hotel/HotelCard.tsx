67ff37b199e1b9400f8ae16177f357f18835c96d
Kittipong Wongasditsayanon <kittipong.wong00@gmail.com>
2025-04-16 22:56:33 +0700

    fix bug

--- components/Home/Hotel/HotelCard.tsx
+++ components/Home/Hotel/HotelCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import { FaHeart } from "react-icons/fa";
 import Image from "next/image";
 import { motion, AnimatePresence } from "framer-motion";
